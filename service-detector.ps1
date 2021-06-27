ee8095b6bd0ad900d1effb5e461aeb9d5f9377d8
Ali Rabiei <68955283+ali-rabiei1989@users.noreply.github.com>
2021-06-27 08:59:00 +0430

    add header to file

--- service-detector.ps1
+++ service-detector.ps1
@@ -1,4 +1,8 @@
-﻿$servers = get-content  ".\machines.txt"
+# Author : Ali Rabiei
+# Creation Date : 11/18/2020
+# This script create a https listener for winrm service and add the firewall rule 
+# --------------------------------------
+$servers = get-content  ".\machines.txt"
 foreach($server in $servers){
 $service = get-service -ComputerName $server w3svc -ErrorAction Ignore
 if($service)
