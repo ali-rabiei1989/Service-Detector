059e29488acea0a0b222c76bab410277fcd67737
Ali Rabiei <68955283+ali-rabiei1989@users.noreply.github.com>
2021-06-27 08:56:36 +0430

    Initial commit

---
+++ service-detector.ps1
@@ -0,0 +1,11 @@
+﻿$servers = get-content  ".\machines.txt"
+foreach($server in $servers){
+$service = get-service -ComputerName $server w3svc -ErrorAction Ignore
+if($service)
+{
+Write-Host "IIS installed on $server"
+} 
+else {
+Write-Host "IIS is not installed on $server"
+}
+}
